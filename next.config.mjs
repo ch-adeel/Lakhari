ccc3d8c3fd0db8017fe56611f67c840a40d30019
Adeel-Hussain <71441933+ch-adeel@users.noreply.github.com>
2025-10-02 00:32:10 +0500

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true, // Disable ESLint during build process
+  },
+};
+
+export default nextConfig;
